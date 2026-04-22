95caa55e0c37abaee97099e36e223e03d4286396
kriti-a567 <kriti.a.64@gmail.com>
2026-04-22 08:22:55 -0700

    Moved module load above uv sync in setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -2,7 +2,7 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
 # Create and activate virtual environment
+module load gcc/14.2.0
 uv sync
 uv sync --dev
 source .venv/bin/activate
-module load gcc/14.2.0
